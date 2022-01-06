aecbf5234da2775099821f5d8074674241e12603
Ravi Akella <raviakella03@outlook.com>
2022-01-06 21:40:36 +0000

    Initial Commit

---
+++ src/main/java/com/ravi/interview/businessrulesengine/BusinessrulesengineApplication.java
@@ -0,0 +1,13 @@
+package com.ravi.interview.businessrulesengine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BusinessrulesengineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BusinessrulesengineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ravi/interview/businessrulesengine/BusinessrulesengineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ravi.interview.businessrulesengine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BusinessrulesengineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
